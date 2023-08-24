3b767bc2836995eb826f92df3e90352999be811e
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2023-08-24 17:37:45 +0800

    consumable

--- src/rcm/RCM.java
+++ src/rcm/RCM.java
@@ -1,10 +1,11 @@
 package rcm;
 
 import mindustry.mod.*;
+import rcm.content.*;
 
 public class RCM extends Mod{
     @Override
     public void loadContent(){
-        
+        RCBlocks.load();
     }
 }
