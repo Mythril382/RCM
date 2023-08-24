d5d33f46334b9d9a7fc6d7683bb09390b87e12c2
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2023-08-24 17:35:16 +0800

    first ever floor

---
+++ src/rcm/content/RCBlocks.java
@@ -0,0 +1,13 @@
+package rcm.content;
+
+import mindustry.world.*;
+import mindustry.world.environment.*;
+
+public class RCBlocks{
+    public static Block
+    cherryGrass;
+
+    public static void load(){
+        cherryGrass = new Floor("cherry-grass");
+    }
+}
