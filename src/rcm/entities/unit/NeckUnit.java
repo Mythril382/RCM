d2c65be64267659640c34df35f19951b1a6c01ea
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2023-08-25 18:56:42 +0800

    error not expected

---
+++ src/rcm/entities/unit/NeckUnit.java
@@ -0,0 +1,38 @@
+package rcm.entities.unit;
+
+import arc.math.*;
+import arc.util.*;
+import mindustry.gen.*;
+import rcm.type.unit.*;
+
+public class NeckUnit extends LegsUnit{
+    public float neckResetTimer = 0f;
+    public float neckRot;
+    
+    @Override
+    public void add(){
+        super.add();
+        neckRot = rotation;
+    }
+    
+    @Override
+    public void unloaded(){
+        super.unloaded();
+        neckRot = rotation;
+    }
+    
+    @Override
+    public void update(){
+        super.update();
+        neckResetTimer += Time.delta;
+        if(moving()) neckResetTimer = 0f;
+        
+        NeckUnitType neck = (NeckUnitType)type;
+        neckRot = Angles.clampRange(neckRot, rotation, neck.neckTrns);
+        
+        if(neckResetTimer > neck.neckReset){
+            neckRot = Angles.moveToward(neckRot, rotation, neck.neckSperd);
+            neckResetTimer = 0f;
+        }
+    }
+}
