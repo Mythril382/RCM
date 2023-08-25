a907641c8c8e8ad0c8b6ede10f4b34f61bc055e4
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2023-08-25 22:58:38 +0800

    null

--- src/rcm/content/RCUnitTypes.java
+++ src/rcm/content/RCUnitTypes.java
@@ -1,6 +1,10 @@
 package rcm.content;
 
+import arc.func.*;
 import arc.graphics.*;
+import arc.struct.*;
+import arc.struct.ObjectMap.*;
+import mindustry.gen.*;
 import mindustry.graphics.*;
 import mindustry.type.*;
 import rcm.entities.unit.*;
@@ -30,5 +34,40 @@ public class RCUnitTypes{
             outlineColor = Color.valueOf("332480");
             constructor = NeckUnit::create;
         }};
+    }
+    
+    //Steal from Hearth which stole from Progressed Materials which stole from Endless Rusting which stole from Progressed Materials in the past which stole from BetaMindy
+    private static final Entry<Class<? extends Entityc>, Prov<? extends Entityc>>[] types = new Entry[]{
+        prov(NeckUnit.class, NeckUnit::new)
+    };
+
+    private static final ObjectIntMap<Class<? extends Entityc>> idMap = new ObjectIntMap<>();
+
+    /** Internal function to flatmap {@code Class -> Prov} into an {@link Entry}.
+     @author GlennFolker */
+    private static <T extends Entityc> Entry<Class<T>, Prov<T>> prov(Class<T> type, Prov<T> prov){
+        Entry<Class<T>, Prov<T>> entry = new Entry<>();
+        entry.key = type;
+        entry.value = prov;
+        return entry;
+    }
+
+    /** Setups all entity IDs and maps them into {@link EntityMapping}.
+     @author GlennFolker */
+    private static void setupID(){
+        for(int i = 0, j = 0, len = EntityMapping.idMap.length; i < len; i++){
+            if(EntityMapping.idMap[i] == null){
+                idMap.put(types[j].key, i);
+                EntityMapping.idMap[i] = types[j].value;
+
+                if(++j >= types.length) break;
+            }
+        }
+    }
+
+    /** Retrieves the class ID for a certain entity type.
+     @author GlennFolker */
+    public static <T extends Entityc> int classID(Class<T> type){
+        return idMap.get(type, -1);
     }
 }
