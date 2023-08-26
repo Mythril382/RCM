8e8b0d5d93172d2317f572934dd778292f06d72b
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2023-08-26 18:48:41 +0800

    sand

--- src/rcm/content/RCBlocks.java
+++ src/rcm/content/RCBlocks.java
@@ -6,15 +6,23 @@ import mindustry.world.blocks.environment.*;
 public class RCBlocks{
     public static Block
     cherryGrass,
+    cherrySand,
     
     cherryWall,
+    cherryDune,
     
     cherryTush;
 
     public static void load(){
         cherryGrass = new Floor("cherry-grass");
 
+        cherrySand = new Floor("cherry-sand");
+
         cherryWall = new StaticWall("cherry-wall"){{
+            variants = 3;
+        }};
+
+        cherryDune = new StaticWall("cherry-dune"){{
             variants = 3;
         }};
 
