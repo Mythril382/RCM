614e7e205876959a4bc9f48b9d9eff1efb9fceda
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2023-08-30 19:56:03 +0800

    forgor²

--- src/rcm/RCM.java
+++ src/rcm/RCM.java
@@ -6,6 +6,7 @@ import rcm.content.*;
 public class RCM extends Mod{
     @Override
     public void loadContent(){
+        RCStatusEffects.load();
         RCUnitTypes.load();
         RCBlocks.load();
     }
