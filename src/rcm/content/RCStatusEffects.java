b7f4b8be1c22268683131c5c058b0a2b64c968f5
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2023-08-30 19:48:02 +0800

    h

---
+++ src/rcm/content/RCStatusEffects.java
@@ -0,0 +1,19 @@
+package rcm.content;
+
+import arc.graphics.*;
+import mindustry.type.*;
+import rcm.type.status.*;
+
+public class RCStatusEffects{
+    public static StatusEffect
+    weak;
+
+    public static void load(){
+        weak = new SpecificStatusEffect("weak"){{
+            speedMultiplier = 0.6f;
+            healthMultiplier = 0.8f;
+            color = Color.valueOf("b5b5b5");
+            targets.add(RCUnitTypes.neckranea, RCUnitTypes.lightBug);
+        }};
+    }
+}
