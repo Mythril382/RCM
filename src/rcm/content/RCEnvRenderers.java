2a5a3d1589760c1908d1b9a78c1e7beb0b4891b9
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2023-08-31 14:10:02 +0800

    why

--- src/rcm/content/RCEnvRenderers.java
+++ src/rcm/content/RCEnvRenderers.java
@@ -22,7 +22,7 @@ public class RCEnvRenderers{
     public static Color vibrantColor(){
         float time = state.rules.ambientLight.a;
         Color color = new Color();
-        color.lerp(new Color[]{Color.valueOf("00000000"), Color.valueOf("00000000"), Color.valueOf("f5b56c80"), Color.valueOf("31316380")}, time);
+        color.lerp(new Color[]{Color.valueOf("000000").a(0f), Color.valueOf("000000").a(0f), Color.valueOf("f5b56c").a(0.4f), Color.valueOf("313163").a(0.4f)}, time);
         return color;
     }
 }
