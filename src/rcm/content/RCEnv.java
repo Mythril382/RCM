df9c6ccfb1b9b5f88750030a930a787f536fc3f5
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2023-08-31 13:50:08 +0800

    env

---
+++ src/rcm/content/RCEnv.java
@@ -0,0 +1,7 @@
+package rcm.content;
+
+public class RCEnv{
+    public static int
+    /** Has vibrant lighting colors. */
+    vibrant = 1 << 8;
+}
---
+++ src/rcm/content/RCEnvRenderers.java
@@ -0,0 +1,28 @@
+package rcm.content;
+
+import arc.*;
+import arc.graphics.*;
+import arc.graphics.g2d.*;
+import mindustry.graphics.*;
+
+import static mindustry.Vars.*;
+
+public class RCEnvRenderers{
+    public static void init(){
+        renderer.addEnvRenderer(RCEnv.vibrant, () -> {
+            Draw.z(Layer.light + 1f);
+            Draw.color(vibrantColor());
+            
+            Fill.rect(Core.camera.position.x, Core.camera.position.y, Core.camera.width, Core.camera.height);
+            
+            Draw.reset();
+        });
+    }
+    
+    public static Color vibrantColor(){
+        float time = Vars.state.rules.ambientColor.a;
+        Color color = new Color();
+        color.lerp(new Color[]{Color.valueOf("00000000"), Color.valueOf("00000000"), Color.valueOf("f5b56c80"), Color.valueOf("31316380")}, time);
+        return color;
+    }
+}
