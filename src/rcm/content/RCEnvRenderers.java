cd77c57c8e90805512e3fdf2022d93d123933254
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2023-09-20 17:13:04 +0800

    unscrewed

--- src/rcm/content/RCEnvRenderers.java
+++ src/rcm/content/RCEnvRenderers.java
@@ -10,6 +10,7 @@ import static mindustry.Vars.*;
 public class RCEnvRenderers{
     public static void init(){
         renderer.addEnvRenderer(RCEnv.vibrant, () -> {
+            if(!state.rules.lighting) return;
             Draw.z(Layer.light + 1f);
             Draw.color(vibrantColor());
             Draw.rect(Core.atlas.find("rcm-big-square"), Core.camera.position.x, Core.camera.position.y, Core.camera.width, Core.camera.height);
@@ -20,7 +21,7 @@ public class RCEnvRenderers{
     public static Color vibrantColor(){
         float time = state.rules.ambientLight.a;
         Color color = new Color();
-        color.lerp(new Color[]{Color.valueOf("edb4b9"), Color.valueOf("ff6373"), Color.valueOf("4136ff")}, time);
+        color.lerp(new Color[]{Color.valueOf("dcdcdc"), Color.valueOf("ff6373"), Color.valueOf("4136ff")}, time);
         return color;
     }
 }
