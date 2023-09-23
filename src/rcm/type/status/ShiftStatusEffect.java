545b330fae1166ca146e3c137cf67792c6ccd9b9
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2023-09-23 13:03:49 +0800

    trueee

--- src/rcm/type/status/ShiftStatusEffect.java
+++ src/rcm/type/status/ShiftStatusEffect.java
@@ -21,7 +21,7 @@ public class ShiftStatusEffect extends StatusEffect{
     public void update(Unit unit, float time){
         super.update(unit, time);
         
-        Unit closest = Units.closestEnemy(unit.team, unit.x, unit.x, range, u -> {});
+        Unit closest = Units.closestEnemy(unit.team, unit.x, unit.x, range, u -> true);
         if(closest != null){
             unshift.spawn(unit.team, unit.x, unit.y);
             unit.kill();
