f00cef73a0df1082672bac144e73a662676073f9
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2023-10-02 04:48:39 +0800

    normal

--- src/rcm/content/RCFx.java
+++ src/rcm/content/RCFx.java
@@ -9,7 +9,7 @@ import static arc.graphics.g2d.Draw.*;
 public class RCFx{
     public static Effect
 
-    droneBlade = new Effect(360f / 6f, e -> {
+    droneBlade = new Effect(360f / 10f, e -> {
         color(e.color);
         rect(Core.atlas.find("rcm-blade"), e.x, e.y, e.fin() * 360f);
         reset();
