622b48ef441bbdf15fe93daacc2ae73540072a53
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2023-10-05 21:48:07 +0800

    funnies

--- src/rcm/RCM.java
+++ src/rcm/RCM.java
@@ -1,11 +1,15 @@
 package rcm;
 
 import arc.*;
+import arc.util.*;
 import mindustry.game.EventType.*;
 import mindustry.mod.*;
 import rcm.content.*;
+import rcm.ui.dialogs.*;
 
 public class RCM extends Mod{
+    public static BestiaryDialog bestiary;
+    
     @Override
     public void loadContent(){
         RCStatusEffects.load();
@@ -14,6 +18,12 @@ public class RCM extends Mod{
 
         Events.on(ClientLoadEvent.class, e -> {
             RCEnvRenderers.init();
+
+            Time.runTask(7f, this::loadUI);
         });
+    }
+
+    public void loadUI(){
+        bestiary = new BestiaryDialog();
     }
 }
